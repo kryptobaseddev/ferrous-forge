26f2327231aee0b4feed4fbe0e4503f06b2ba2d4
kryptobaseddev <kryptobaseddev@users.noreply.github.com>
2026-01-25 00:40:50 +0000

    deploy: 81e6b867cdbd415799a49c5ebf16ea09ada71398

--- api/trait.impl/core/hash/trait.Hash.js
+++ api/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ferrous_forge",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"ferrous_forge/validation/violation/enum.ViolationType.html\" title=\"enum ferrous_forge::validation::violation::ViolationType\">ViolationType</a>"]]]]);
+    var implementors = Object.fromEntries([["ferrous_forge",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"ferrous_forge/validation/violation/enum.ViolationType.html\" title=\"enum ferrous_forge::validation::violation::ViolationType\">ViolationType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
